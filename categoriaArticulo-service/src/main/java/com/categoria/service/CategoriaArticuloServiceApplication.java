e32ee7b9d08699b70a44b178a2cdff447f16c5f0
MagDev <iscmagc24@gmail.com>
2022-05-12 13:56:37 -0500

    iniciando Config-Server

---
+++ categoriaArticulo-service/src/main/java/com/categoria/service/CategoriaArticuloServiceApplication.java
@@ -0,0 +1,13 @@
+package com.categoria.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CategoriaArticuloServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CategoriaArticuloServiceApplication.class, args);
+	}
+
+}
---
+++ categoriaArticulo-service/src/test/java/com/categoria/service/CategoriaArticuloServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.categoria.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CategoriaArticuloServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
