18677ae56de9ddf348bd246bb549a5a0d1353c78
MagDev <iscmagc24@gmail.com>
2022-05-16 12:09:44 -0500

    Creacion y configuracion de microservicio Proveedores

---
+++ proveedores-service/src/test/java/com/proveedores/service/ProveedoresServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proveedores.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProveedoresServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
